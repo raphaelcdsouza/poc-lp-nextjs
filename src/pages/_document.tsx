b1544fadc6268ac7672ebc6bfceacee846d24e1b
Raphael Cássio de Souza <raphaelsouza.qintess@natura.net>
2020-10-11 20:19:33 -0300

    Adjusted font call for better performance

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,9 +8,44 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <title>Joinville Implementos</title>
           <meta name="description" content="A melhor maneira de ampliar sua frota"/>
-
-          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet" />
-          <link rel="shortcut icon" href="favicon.svg" type="image/svg+xml"/>
+          <style dangerouslySetInnerHTML={{
+            __html: `
+              body, input, button {
+                font-family: 'Montserrat', sans-serif;
+                font-size: 16px;
+              }
+            `
+          }} />
+          <style dangerouslySetInnerHTML={{
+            __html: `
+              @font-face {
+                font-family: 'Montserrat';
+                font-style: normal;
+                font-weight: 400;
+                font-display: swap;
+                src: local('Montserrat Regular'), local('Montserrat-Regular'), url(https://fonts.gstatic.com/s/montserrat/v15/JTUSjIg1_i6t8kCHKm459WlhyyTh89Y.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+              }
+    
+              @font-face {
+                font-family: 'Montserrat';
+                font-style: normal;
+                font-weight: 500;
+                font-display: swap;
+                src: local('Montserrat Medium'), local('Montserrat-Medium'), url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_ZpC3gnD_vx3rCs.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+              }
+    
+              @font-face {
+                font-family: 'Montserrat';
+                font-style: normal;
+                font-weight: 700;
+                font-display: swap;
+                src: local('Montserrat Bold'), local('Montserrat-Bold'), url(https://fonts.gstatic.com/s/montserrat/v15/JTURjIg1_i6t8kCHKm45_dJE3gnD_vx3rCs.woff2) format('woff2');
+                unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+              }
+            `
+          }} />
         </Head>
 
         <body>
