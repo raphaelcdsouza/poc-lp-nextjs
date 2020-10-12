2fc2f023340d01e8c451d0c90380a53a57951a45
Raphael Cássio de Souza <raphaelsouza.qintess@natura.net>
2020-10-12 10:30:21 -0300

    Final styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
-    <>
-      <div className="wrapper">
+    <div className="wrapper">
+      <div className="content">
         <main>
-          <picture>
-            <img src="/logojlleimplementos.svg" height="51.23" width="200" alt="Logo Joinville Implementos"/>
-          </picture>
+          <img src="/logojlleimplementos.svg" height="89.67" width="350" alt="Logo Joinville Implementos"/>
 
           <h1>Quais são os principais benefícios do consórcio?</h1>
 
@@ -23,22 +21,22 @@ export default function Home() {
           <p>Preencha os dados para receber um contato da nossa equipe!</p>
           <form>
             <label htmlFor="name">Nome</label>
-            <input type="text" name="name" id="name"/>
+            <input type="text" name="name" id="name" placeholder="Ex.: João da Silva" />
             <label htmlFor="email">E-mail</label>
-            <input type="text" name="email" id="email"/>
+            <input type="text" name="email" id="email" placeholder="Ex.: joao@silva.com.br" />
             <label htmlFor="phone">Telefone</label>
-            <input type="text" name="phone" id="phone"/>
+            <input type="text" name="phone" id="phone" placeholder="DDD + número" />
             <label htmlFor="state">Estado</label>
-            <input type="text" name="state" id="state"/>
+            <input type="text" name="state" id="state" placeholder="Ex.: Santa Catarina" />
             <label htmlFor="city">Cidade</label>
-            <input type="text" name="city" id="city"/>
+            <input type="text" name="city" id="city" placeholder="Ex.: Joinville" />
             <button>Receber contato</button>
           </form>
-          <span>Ao receber o conteúdo você concorda em receber materiais periódicos em seu e-mail.</span>
+          <small>Ao receber o conteúdo você concorda em receber materiais periódicos em seu e-mail.</small>
         </aside>
       </div>
 
-      <footer>Conheça a Joinville implementos</footer>
-    </>
+      <footer><a href="https://www.joinvilleimplementos.com.br">Conheça a Joinville implementos</a></footer>
+    </div>
   )
 }
